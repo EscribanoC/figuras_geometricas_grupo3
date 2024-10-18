ff8e8f7423c1ddbfe3a06fd9921433b08e2d6af3
EscribanoC <escribano.flores.carlos@iescamas.es>
2024-10-18 10:45:27 +0200

    Actualización clase Punto
    He creado la clase Punto con sus atributos, constructor y métodos.

--- punto.php
+++ punto.php
@@ -1,1 +1,33 @@
 <?php
+
+class Punto{
+    //Atributos
+    private $x;
+    private $y;
+
+    //Constructor
+    public function __construct(float $x, float $y){
+        $this -> x = $x;
+        $this -> y = $y;
+    }
+
+    //Getters de la clase
+
+    //Getter del atributo 'x'
+    public function getX(){
+        return $this -> x;
+    }
+
+    //Getter del atributo 'y'
+    public function getY(){
+        return $this -> y;
+    }
+
+    //Métodos de la clase
+
+    //Método para calcular la distancia entre dos puntos
+    public function distancia(Punto $punto){
+        $distancia = sqrt(pow(($punto -> x - $this -> x),2) + pow(($punto -> y - $this -> y),2)); //Calculo la distancia a través del teorema de Pitágoras.
+        return $distancia;
+    }
+}
