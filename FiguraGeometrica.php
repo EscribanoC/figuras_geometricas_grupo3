b5f6ed45427883679cd4e7c955d1f49c9b857cfe
jegoncu <jegoncu@gmail.com>
2024-10-18 10:43:15 +0200

    Implementación clase abstracta FiguraGeometrica

---
+++ FiguraGeometrica.php
@@ -0,0 +1,29 @@
+<?php
+
+// Define los métodos abstractos area() y perimetro(), que serán implementados por las subclases.
+// No se puede instanciar directamente.
+
+abstract class FiguraGeometrica
+{
+  // Atributos:
+  public $nombre;
+
+  // Constructor:
+  public function __construct($nombre)
+  {
+    $this->nombre = $nombre;
+  }
+
+  // Getter
+  public function get_nombre()
+  {
+    return $this->nombre;
+  }
+
+  // Métodos abstractos:
+  // Calcular el área de la figura.
+  abstract public function area();
+
+  // Calcular el perímetro de la figura.
+  abstract public function perimetro();
+}
