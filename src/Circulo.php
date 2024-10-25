9a5f3afd848a4ea7f213fbf9934c60decdcf59f0
jegoncu <jegoncu@gmail.com>
2024-10-25 10:18:01 +0200

    clase circulo

---
+++ src/Circulo.php
@@ -0,0 +1,30 @@
+<?php
+include_once('FiguraGeometrica.php');
+
+// Clase Círculo que extiende FiguraGeometrica
+class Circulo extends FiguraGeometrica{
+    private float $radio;
+    private punto $centro;
+
+    // Constructor que recibe el centro y el radio del círculo
+    public function __construct(punto $centro, float $radio) {
+        $this->centro = $centro;
+        $this->radio = $radio;
+    }
+
+    //Implementación del método getRadio
+    public function getRadio(): float{
+        return $this -> radio;
+    }
+
+    // Implementación del método area
+    public function area(): float {
+        return pi() * pow($this->radio, 2);
+    }
+
+    // Implementación del método perimetro
+    public function perimetro(): float {
+        return 2 * pi() * $this->radio;
+    }
+}
+//comentario
