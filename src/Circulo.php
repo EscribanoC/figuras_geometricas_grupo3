112bd7dbf78db382093f37f0a948a6f198e5389e
jegoncu <jegoncu@gmail.com>
2024-10-30 18:26:27 +0100

    ciruclo tested
    circulo tested

--- src/Circulo.php
+++ src/Circulo.php
@@ -1,29 +1,36 @@
 <?php
-include_once('FiguraGeometrica.php');
+include_once 'FiguraGeometrica.php';
+require_once 'Punto.php';
 
 // Clase Círculo que extiende FiguraGeometrica
-class Circulo extends FiguraGeometrica{
+class Circulo extends FiguraGeometrica
+{
     private float $radio;
-    private punto $centro;
+    private Punto $centro;
 
     // Constructor que recibe el centro y el radio del círculo
-    public function __construct(punto $centro, float $radio) {
+    public function __construct(String $nombre, Punto $centro, float $radio)
+    {
+        parent::__construct($nombre);
         $this->centro = $centro;
         $this->radio = $radio;
     }
 
-    //Implementación del método getRadio
-    public function getRadio(): float{
-        return $this -> radio;
+    // Implementación del método getRadio
+    public function getRadio(): float
+    {
+        return $this->radio;
     }
 
     // Implementación del método area
-    public function area(): float {
+    public function area(): float
+    {
         return pi() * pow($this->radio, 2);
     }
 
     // Implementación del método perimetro
-    public function perimetro(): float {
+    public function perimetro(): float
+    {
         return 2 * pi() * $this->radio;
     }
 }
